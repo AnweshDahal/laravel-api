46c5e0c28fe274b0257c380efbf453c52fe73599
AnweshDahal <anweshd2000@gmail.com>
2021-08-11 10:10:43 +0545

    Adds user login and register

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProductController;
+use App\Http\Controllers\AuthController;
 
 /*
 |--------------------------------------------------------------------------
@@ -18,12 +19,15 @@ use App\Http\Controllers\ProductController;
 // Route::resource('products', ProductController::class);
 
 // Public Routes
+ROUTE::post('/register', [AuthController::class, 'register']);
+ROUTE::post('/login', [AuthController::class, 'login']);
 Route::get('/products', [ProductController::class, 'index']);
 Route::get('/products/{product}', [ProductController::class, 'show']);
 Route::get('/products/search/{name}', [ProductController::class, 'search']);
 
 
 Route::group(['middleware'=>['auth:sanctum']], function() {
+    ROUTE::post('/logout', [AuthController::class, 'logout']);
     ROUTE::post('/products', [ProductController::class, 'store']);
     ROUTE::put('/products/{id}', [ProductController::class, 'update']);
     ROUTE::delete('/products/{id}', [ProductController::class, 'destroy']);
